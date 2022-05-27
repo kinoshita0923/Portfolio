43b39b65c76456ed3206382605c56bc3975533f0
kinoshita0923 <sota0923ks15@gmail.com>
2022-05-28 05:33:09 +0900

    ハンバーガーメニューの動作の修正

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // DOM
-const nav_a = document.getElementsByClassName('page_in');
-let hambureger_chkbox = document.getElementById('hamburger-menu');
+const nav_a = document.querySelectorAll('.page_in');
+const menu = document.querySelector('.menu label');
 
 for (let i = 0; i < nav_a.length; i++) {
     nav_a[i].addEventListener('click', () => {
-        hambureger_chkbox.checked = false;
+        menu.click();
     })
 }
