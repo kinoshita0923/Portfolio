6664990811ec9a71a68e20ccc92d9a1017fafa12
kinoshita0923 <sota0923ks15@gmail.com>
2022-05-28 06:00:11 +0900

    スクロール禁止処理

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 // DOM
 const nav_a = document.querySelectorAll('.page_in');
 const menu = document.querySelector('.menu label');
+const back = document.querySelector('.back');
 
+// 変数
+let count = 0;
+
+// イベント処理
 for (let i = 0; i < nav_a.length; i++) {
     nav_a[i].addEventListener('click', () => {
         menu.click();
-    })
+    });
 }
+
+menu.addEventListener('click', () => {
+    if (count === 0) {
+        count = 1;
+        document.body.classList.add('body');
+    } else {
+        count = 0;
+        document.body.classList.remove('body');
+    }
+});
+
+back.addEventListener('click', () => {
+    count = 0;
+    document.body.classList.remove('body');
+});
